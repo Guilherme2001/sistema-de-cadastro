c5841805aa947b4b51ebe63bc3150d4af71108d1
Guilherme <guial@LAPTOP-FRSLVI6L.com>
2024-05-16 22:50:08 -0300

    console log indicando modificaçõe

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+
+// mais um console log de testes
+console.log("o paulo fez esse commit sem voce ver na aula")
